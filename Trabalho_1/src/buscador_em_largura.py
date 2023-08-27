d0074a3a48f28daaf3ee0e113b06c4506a2a993b
Augusto H. V. Guerner <augusto.h.v.guerner@gmail.com>
2023-08-27 17:31:12 -0300

    Ajuste no metodo buscar

--- Trabalho_1/src/buscador_em_largura.py
+++ Trabalho_1/src/buscador_em_largura.py
@@ -6,7 +6,7 @@ class BuscadorEmLargura:
         niveis = [] # Cada indice é um nivel (lista de lista e não matriz)
         vertices_a_serem_visitados_por_nivel = [vertice]
         vertices_visitados = [vertice]
-        nivel = 1
+        
         while len(vertices_a_serem_visitados_por_nivel):
             vertices = vertices_a_serem_visitados_por_nivel[:]
             vizinhos_por_nivel = []
@@ -19,6 +19,6 @@ class BuscadorEmLargura:
             vertices_visitados.extend(vizinhos_por_nivel[:])
             vertices_a_serem_visitados_por_nivel = vizinhos_por_nivel[:]
             niveis.append(vertices[:])
-            nivel += 1
-        return niveis
+
+        return niveis[:]
             
