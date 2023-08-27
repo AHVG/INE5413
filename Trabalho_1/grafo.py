e888f1d06c3302120a2f4ad820504a9450c35d02
Augusto H. V. Guerner <augusto.h.v.guerner@gmail.com>
2023-08-27 16:31:31 -0300

    Lendo de um arquivo o grafo

--- Trabalho_1/grafo.py
+++ Trabalho_1/grafo.py
@@ -3,8 +3,9 @@
 class Grafo:
 
 
-    def __init__(self, grafo_em_matriz):
-        self.__matriz_de_adjacencia = grafo_em_matriz[:]
+    def __init__(self):
+        self.__rotulos = [] # Não sei para que serve esses rotulos
+        self.__matriz_de_adjacencia = []
 
     def qtdVertices(self):
         return len(self.__matriz_de_adjacencia)
@@ -13,35 +14,41 @@ class Grafo:
         return sum([sum([1 for v in linha[i:] if v != float("Inf")]) for i, linha in enumerate(self.__matriz_de_adjacencia)])
 
     def grau(self, v):
-        return sum(filter(lambda x: x != float("Inf"), self.__matriz_de_adjacencia[v]))
+        return sum(filter(lambda x: x != float("Inf"), self.__matriz_de_adjacencia[v - 1]))
 
     def rotulo(self, v): # Não sei que porra é esta
         pass
 
     def vizinhos(self, v):
-        return [i for i, x in enumerate(self.__matriz_de_adjacencia[v]) if x != float("Inf")]
+        return [i for i, x in enumerate(self.__matriz_de_adjacencia[v - 1]) if x != float("Inf")]
 
     def haAresta(self, u, v):
-        pass
+        return self.__matriz_de_adjacencia[u - 1][v - 1] != float("Inf")
 
     def peso(self, u, v):
-        pass
+        return self.__matriz_de_adjacencia[u - 1][v - 1]
 
     def ler(self, arquivo):
-        pass
+        with open(arquivo, "r") as arq:
+            linhas = arq.read().split("\n")
+        numero_de_vertices = int(linhas[0].split()[1])
+        inicio_leitura_de_vertices = 1
+        inicio_leitura_de_edges = numero_de_vertices + 2
+
+        rotulos = linhas[inicio_leitura_de_vertices:inicio_leitura_de_edges - 1]
+        self.__rotulos = [rotulo for v, rotulo in map(lambda x: x.split(), rotulos)]
+        
+        vertices = linhas[inicio_leitura_de_edges:]
+        self.__matriz_de_adjacencia = [[float("Inf") for __ in range(numero_de_vertices)] for _ in range(numero_de_vertices)]
+        for v, u, p in map(lambda x: map(float, x.split()), vertices):
+            u = int(u) - 1
+            v = int(v) - 1
+            self.__matriz_de_adjacencia[v][u] = p
+            self.__matriz_de_adjacencia[u][v] = p
 
     def __str__(self):
         return str(self.__matriz_de_adjacencia)
 
-g = Grafo([
-[float("Inf"), 1, 1],
-[1, float("Inf"), float("Inf")],
-[1, float("Inf"), float("Inf")]
-])
-
+g = Grafo()
+g.ler("grafos.txt")
 print(g)
-print(g.qtdArestas())
-print(g.grau(1))
-print(g.vizinhos(0))
-print(g.vizinhos(1))
-print(g.vizinhos(2))
