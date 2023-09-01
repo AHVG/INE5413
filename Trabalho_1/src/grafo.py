f383a3774b8e13196100aaa988ee169b4ebd0c25
Augusto H. V. Guerner <augusto.h.v.guerner@gmail.com>
2023-09-01 11:28:15 -0300

    Melhorando codigo

--- Trabalho_1/src/grafo.py
+++ Trabalho_1/src/grafo.py
@@ -2,28 +2,29 @@
 
 class Grafo:
 
+    sem_aresta = float("Inf") # Pode ser qualquer outro valor mágico, mas foi escolhido infinito pq é o mais descritivo
 
     def __init__(self):
-        self.__rotulos = [] # Não sei para que serve esses rotulos
+        self.__rotulos = [] # Não sei para que serve esses rotulos: para nada
         self.__matriz_de_adjacencia = []
 
     def qtdVertices(self):
         return len(self.__matriz_de_adjacencia)
 
     def qtdArestas(self):
-        return sum([sum([1 for v in linha[i:] if v != float("Inf")]) for i, linha in enumerate(self.__matriz_de_adjacencia)])
+        return sum([sum([1 for v in linha[i:] if v != self.sem_aresta]) for i, linha in enumerate(self.__matriz_de_adjacencia)])
 
     def grau(self, v):
-        return sum(filter(lambda x: x != float("Inf"), self.__matriz_de_adjacencia[v - 1]))
+        return len(self.vizinhos(v))
 
     def rotulo(self, v): # Não sei que porra é esta
         return self.__rotulos[v - 1]
 
     def vizinhos(self, v):
-        return [i + 1 for i, x in enumerate(self.__matriz_de_adjacencia[v - 1]) if x != float("Inf")]
+        return [i + 1 for i, x in enumerate(self.__matriz_de_adjacencia[v - 1]) if x != self.sem_aresta]
 
     def haAresta(self, u, v):
-        return self.__matriz_de_adjacencia[u - 1][v - 1] != float("Inf")
+        return self.__matriz_de_adjacencia[u - 1][v - 1] != self.sem_aresta
 
     def peso(self, u, v):
         return self.__matriz_de_adjacencia[u - 1][v - 1]
@@ -40,7 +41,7 @@ class Grafo:
         self.__rotulos = [rotulo for v, rotulo in map(lambda x: x.split(), rotulos)]
         
         vertices = linhas[inicio_leitura_de_edges:]
-        self.__matriz_de_adjacencia = [[float("Inf") for __ in range(numero_de_vertices)] for _ in range(numero_de_vertices)]
+        self.__matriz_de_adjacencia = [[self.sem_aresta] * numero_de_vertices for _ in range(numero_de_vertices)]
         for v, u, p in map(lambda x: map(float, x.split()), vertices):
             u = int(u) - 1
             v = int(v) - 1
