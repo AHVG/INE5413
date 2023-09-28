88c74b5653209f0173e3ca04024f04d6c191fdcc
Augusto H. V. Guerner <augusto.h.v.guerner@gmail.com>
2023-09-27 22:30:23 -0300

    comentários

--- Trabalho_1/src/buscador_em_largura.py
+++ Trabalho_1/src/buscador_em_largura.py
@@ -9,15 +9,19 @@ class BuscadorEmLargura:
         vertices_visitados = [vertice]
         
         while len(vertices):
+            # Lista de todos os vizinhos de cada vertice da lista de vertices
             vizinhos = []
             for v in vertices:
+                # filtra todos vertices ainda não visitados que são vizinhos dos vertices da lista de vertices
                 novos_vertices = list(filter(lambda x: x not in vertices_visitados, grafo.vizinhos(v)))
+                # adiciona na lista de vertices visitados esses novos vertices e na lista de vizinhos
                 vertices_visitados.extend(novos_vertices)
                 vizinhos.extend(novos_vertices)
 
-            vertices_visitados.extend(vizinhos[:])
+            # Adiciona mais um nível e atualiza os a lista de vertices para agora ser a lista de vizinhos dos vertices
             niveis.append(vertices[:])
             vertices = vizinhos[:]
 
+        # retorna os níveis
         return niveis[:]
             
