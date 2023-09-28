bb43df232603ae5462e228074b9f463563cbd2b4
Augusto H. V. Guerner <augusto.h.v.guerner@gmail.com>
2023-09-27 23:04:28 -0300

    Beijo na bunda até segunda

--- Trabalho_1/src/3.py
+++ Trabalho_1/src/3.py
@@ -3,14 +3,21 @@ from ciclo_euleriano import CicloEuleriano
 import argparse
 
 def main():
+    # Paramêtro de programa
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', '--file', action='store', type=str, required=True, help="Path do arquivo que tem o grafo")
     args = parser.parse_args()
 
+    # Cria um grafo não direcionado e ponderado (pode ser ponderado)
     grafo = Grafo(False, True)
     grafo.ler(args.file)
-    tem, ciclo = CicloEuleriano().hierholzer(grafo)
-    print(tem, ciclo)
 
+    # Busca o ciclo euleriano e mostra na tela o resultado
+    ciclo = CicloEuleriano().hierholzer(grafo)
+    if ciclo:
+        print(1)
+        print((", ".join([str(v) for v in ciclo])))
+    else:
+        print(0)
 
 main()
--- Trabalho_1/src/4.py
+++ Trabalho_1/src/4.py
@@ -3,14 +3,18 @@ from dijkstra import Dijkstra
 import argparse
 
 def main():
+    # Paramêtro de programa
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', '--file', action='store', type=str, required=True, help="Path do arquivo que tem o grafo")
     parser.add_argument('-v', '--vertice', action='store', type=int, required=True, help="Vertice que será analisado")
     args = parser.parse_args()
 
+    # Cria um grafo direcionado e ponderado (pode ser não direcionado)
     grafo = Grafo(True, True)
     grafo.ler(args.file)
     inicio = args.vertice
+
+    # Faz a busca e mostra na tela
     niveis = Dijkstra().busca(grafo, inicio)
     for no in niveis:
         lista_antecessores = Dijkstra().getAntecessores(no)
