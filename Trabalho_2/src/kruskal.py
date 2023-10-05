1536a838854b43c4bef84decb659e51e2dc0897c
Micael <mikal2003.sp@gmail.com>
2023-10-05 10:52:50 -0300

    deixando mais simples

--- Trabalho_2/src/kruskal.py
+++ Trabalho_2/src/kruskal.py
@@ -1,17 +1,17 @@
 from grafo import Grafo
 
-class Kruskal:
+# Numero de arestas da arvore geradora tem de ser numero de vertices -1
+# Arestas nao podem formar ciclos
+# Arestas tem de ser as de menor peso possivel
+# Arestas tem de ligar todos os vertices
+# Grafo tem de ser nao direcionado
 
-    def uniao(self, A,  B):
-        x = []
-        for a in A:
-            x.append(a)
-        for b in B:
-            x.append(b)
-        return x
+
+class Kruskal:
     
     def busca(self, grafo: Grafo):
         a = set()
+        #estrutura para identificacao da arvore em cada vertice
         s = [{i} for i in range(1, grafo.qtdVertices() + 1)]
 
         arestas = grafo.obterArestas()
@@ -22,10 +22,11 @@ class Kruskal:
         #ordenar o dicionario pela ordem crescente de peso
         ordered_dic = dict(sorted(dic.items(), key=lambda item: item[1]))
 
+        # Atualizacao da identidade das arvores
         for u,v in ordered_dic.keys():
             if s[u - 1] != s[v - 1]:
                 a.add((u,v))
-                x = self.uniao(s[u - 1], s[v - 1])
+                x = s[u-1].union(s[v-1])
                 for y in x:
-                    s[y - 1] = x
+                    s[y-1] = x
         return a
